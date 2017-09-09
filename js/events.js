703d9fbe94adab00407a9137e6452a460a4b301a
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-09 03:52:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,41 @@
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
+pressIt()
+submitIt()
+frameIt()
 
 });
+
+function getIt() {
+  $('p').on('click', function(event) {
+    alert('I was clicked')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+// this also works...
+// function frameIt() {
+//     $('img').addClass('tasty')
+// }
+
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('You have pressed G');
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
