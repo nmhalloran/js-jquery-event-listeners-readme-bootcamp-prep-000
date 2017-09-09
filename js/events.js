ce8a14139c69ad8fdc7615b07af2a0e66861f477
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-09 03:33:46 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,30 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt() {
+  $('p').on('click', function(event) {
+    alert('I was clicked')
+  })
+}
+
+function frameIt() {
+  $('img')on('load', function() {
+    $('img')addClass('tasty')
+
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('You have pressed G');
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
